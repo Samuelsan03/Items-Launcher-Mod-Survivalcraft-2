0e87f78ba3d25b2c82b54e0c09eedb776d9320bf
Samuelsan03 <113067357+Samuelsan03@users.noreply.github.com>
2026-01-03 21:22:44 -0500

    Add files via upload

---
+++ ModItemsLauncher.cr
@@ -0,0 +1,8 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<ModItemsLauncher>
+	<Weapons>
+		<Recipe Result="ItemsLauncherBlock" ResultCount="1" RequiredHeatLevel="0" RequiredPlayerLevel="10" a="copperblock" b="musket" c="rod" Description="Haz un lanzador de objetos modificando un mosquete nuevo.">
+			"bca"
+		</Recipe>
+	</Weapons>
+</ModItemsLauncher>
